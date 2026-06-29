b798230f8b7242b5af1d986872431d606c58a396
Tom Knight <tommbk50@gmail.com>
2026-06-29 11:34:31 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/atom-arc-65caf85b/',
   server: {
     host: "::",
     port: 8080,
